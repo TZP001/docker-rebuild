6a28c2ad0aed22e4dcc55858c5069507bfab0d65
TZP001 <145636057+TZP001@users.noreply.github.com>
2024-11-17 14:39:04 +0800

    Update and rename novnc.sh to install_deb.sh

---
+++ diy/debian/install_deb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+deb_file="$1"
+
+# 查询依赖
+dependencies=$(dpkg -I "$deb_file" | grep "Depends:" | awk -F ':' '{print $2}' | tr ',' '\n')
+
+# 输出依赖项
+echo "依赖项：${dependencies[@]}"
+
+echo "开始安装"
+
+for dependency in $dependencies; do
+    apt-get install -y "$dependency"
+done
