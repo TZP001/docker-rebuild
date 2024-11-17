6e3e2e3331bf5bb46c91580f84e0cb44d1a32f06
TZP001 <145636057+TZP001@users.noreply.github.com>
2024-11-17 16:34:21 +0800

    Update install_deb.sh

--- debian/slim/install_deb.sh
+++ debian/slim/install_deb.sh
@@ -8,8 +8,11 @@ dependencies=$(dpkg -I "$deb_file" | grep "Depends:" | awk -F ':' '{print $2}' |
 # 输出依赖项
 echo "依赖项：${dependencies[@]}"
 
-echo "开始安装"
+echo "开始安装依赖"
 
 for dependency in $dependencies; do
     apt-get install -y "$dependency"
 done
+
+echo "开始安装主程序"
+dpkg -i "$deb_file"
