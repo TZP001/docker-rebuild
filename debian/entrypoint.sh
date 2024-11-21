80daff3aec767f292dd3f3b1db4ae980ffa89a3b
TZP001 <145636057+TZP001@users.noreply.github.com>
2024-11-21 19:25:19 +0800

    Update entrypoint.sh

--- debian/entrypoint.sh
+++ debian/entrypoint.sh
@@ -5,20 +5,43 @@ figlet Diy Debian
 echo -e "======================1. 检测自定义环境变量文件========================\n"
 
 DIY_ENV_FILE="/debian/scripts/env"
-echo  -e "NOVNCPort、VNC_USERNAME、VNC_PASSWPRD、RUN_RPO可以在“$DIY_ENV_FILE”中重新设置，使用export AA=""设置"
+echo  -e "NOVNCPort、VNC_USERNAME、VNC_PASSWPRD、RUN_RPO可以在“$DIY_ENV_FILE”中重新设置，使用export AA="" 设置"
 if [ -f "$DIY_ENV_FILE" ]; then
-  echo -e "检测到$DIY_ENV_FILE 文件，重新设置相应环境变量\n"
-  bash "$DIY_ENV_FILE"
+    echo -e "检测到$DIY_ENV_FILE 文件，重新设置相应环境变量\n"
+    # 读取 env 文件
+    while IFS= read -r line; do
+        # 提取变量名和值
+        var=$(echo "$line" | cut -d'=' -f1 | cut -d' ' -f2)
+        value=$(echo "$line" | cut -d'=' -f2)
+    
+        # 判断变量是否存在且值是否相等
+        if [ -z "${!var}" ] || [ "${!var}"!= "$value" ]; then
+            sed -i "/export $var=/d"  ~/.bashrc
+            echo "export $var=$value" >> ~/.bashrc
+    	fi
+    done < "$DIY_ENV_FILE"
 elif [ ! -f "$DIY_ENV_FILE" ]; then
-  echo -e "自定义环境变量文件$DIY_ENV_FILE 不存在\n"
+    echo -e "自定义环境变量文件$DIY_ENV_FILE 不存在\n"
 fi
+
+# 设置屏幕
+echo 'export DISPLAY=$(hostname)":1"' >>  ~/.bashrc
+# 设置用户名
+sed -i "/export USER=/d"  ~/.bashrc
+echo "export USER=${VNC_USERNAME:-VNCC}" >> ~/.bashrc
+
+# 环境变量立即生效
+source ~/.bashrc
+
+# 获取环境变量
+NOVNCPort="${NOVNCPort:-5800}"
+vncpwd="${VNC_PASSWPRD:-123456}"
+DIY_RUSN_SH="${DIY_RUSN_SH}"
+RUN_RPO="${RUN_RPO}"
 
 echo -e "======================2. 设置远程桌面账号和密码========================\n"
 
-NOVNCPort="${NOVNCPort:-5800}"
 # 自动设置VNC密码
-export USER="${VNC_USERNAME:-VNCC}"
-vncpwd="${VNC_PASSWPRD:-123456}"
 echo -e "设置vnc密码\n"
 expect << EOF
 	spawn vncpasswd
@@ -39,12 +62,9 @@ tightvncserver :1 -geometry 1600x900 -depth 24 -port 5901
 echo "" > '/usr/share/novnc/Click 【vnc.html】!!! NOT ME!!!'
 websockify -D --web=/usr/share/novnc $NOVNCPort localhost:5901
 
-# 设置屏幕
-export DISPLAY=$(hostname)":1"
-
 echo -e "======================3. 检测自定义脚本是否存在========================\n"
 
-DIY_RUSN_SH="${DIY_RUSN_SH}"
+
 if [ "$DIY_RUSN_SH" != "" ] && [ -f "$DIY_RUSN_SH" ]; then
   echo -e "执行自定义脚本$DIY_RUSN_SH，后台运行 \n"
   "$DIY_RUSN_SH" &
@@ -57,7 +77,6 @@ fi
 echo -e "======================4. 运行镜像内部自定义程序========================\n"
 
 program="/usr/bin/grass"
-RUN_RPO="${RUN_RPO}"
 
 if [ -f "$program" ] && [ "$RUN_RPO" = "true" ]; then
   # 文件存在，执行文件
